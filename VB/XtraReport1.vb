ba802691d9bae5ec9780b32b8dcfb7ac30ad57ff
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/XtraReport1.vb
@@ -0,0 +1,10 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace LimitNumberOfRecordsPerPage
+	Partial Public Class XtraReport1
+		Inherits DevExpress.XtraReports.UI.XtraReport
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
