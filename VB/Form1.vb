7b537a054d7aabca7d340a031a41d2c5d725213b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UI
+
+Namespace LimitNumberOfRecordsPerPage
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+            Dim detailreport As New XtraReport1()
+            Dim printTool As New ReportPrintTool(detailreport)
+            printTool.ShowPreview()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/XtraReport1.vb
@@ -0,0 +1,9 @@
+﻿Namespace LimitNumberOfRecordsPerPage
+    Partial Public Class XtraReport1
+        Inherits DevExpress.XtraReports.UI.XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
